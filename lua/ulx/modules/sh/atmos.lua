f4a22c4bb0adcec6a4e57d9694f1ed9e2dc5ed2d
viral32111 <19510403+viral32111@users.noreply.github.com>
2020-03-15 11:59:52 +0000

    Added ULX code

---
+++ lua/ulx/modules/sh/atmos.lua
@@ -0,0 +1,48 @@
+local categoryName="Atmos"
+
+local toggletime=ulx.command(categoryName,"ulx toggletime",function(ply)
+	if(SERVER==true)then
+		ply:ConCommand("atmos_dnc_pause")
+	end
+	ulx.fancyLogAdmin(ply,"#A toggled the time.")
+end,{"!toggletime"},true)
+toggletime:defaultAccess(ULib.ACCESS_ADMIN)
+toggletime:help("Toggles (pauses/resumes) the time.")
+
+local settime=ulx.command(categoryName,"ulx settime",function(ply,time)
+	if(SERVER==true)then
+		ply:ConCommand("atmos_dnc_settime "..tostring(math.Clamp(math.Round(time),0,24)))
+	end
+	ulx.fancyLogAdmin(ply,"#A set the time to #s.",time)
+end,{"!settime"},true)
+settime:defaultAccess(ULib.ACCESS_ADMIN)
+settime:addParam{type=ULib.cmds.NumArg,hint="Time (24-hour)",min=0,max=24}
+settime:help("Sets the time.")
+
+local setstorm=ulx.command(categoryName,"ulx setstorm",function(ply,enabled)
+	if(SERVER==true)then
+		if(enabled==true)then
+			ply:ConCommand("atmos_startstorm")
+		else
+			ply:ConCommand("atmos_stopstorm")
+		end
+	end
+	ulx.fancyLogAdmin(ply,"#A "..(enabled==true&&"enabled"||"disabled").." a storm.")
+end,{"!setstorm"},true)
+setstorm:defaultAccess(ULib.ACCESS_ADMIN)
+setstorm:addParam{ type=ULib.cmds.BoolArg,hint="Enable storm?"}
+setstorm:help("Enables or disables a storm.")
+
+local setsnow=ulx.command(categoryName,"ulx setsnow",function(ply,enabled)
+	if(SERVER==true)then
+		if(enabled==true)then
+			ply:ConCommand("atmos_startsnow")
+		else
+			ply:ConCommand("atmos_stopsnow")
+		end
+	end
+	ulx.fancyLogAdmin(ply,"#A "..(enabled==true&&"enabled"||"disabled").." a snow storm.")
+end,{"!setsnow"},true)
+setsnow:defaultAccess(ULib.ACCESS_ADMIN)
+setsnow:addParam{ type=ULib.cmds.BoolArg,hint="Enable snow?"}
+setsnow:help("Enables or disables a snow storm.")
