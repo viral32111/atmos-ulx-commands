e95abcd6d7cbe1264fb526f10633a34adef4b333
viral32111 <19510403+viral32111@users.noreply.github.com>
2021-01-01 12:12:50 +0000

    Update copyright notice to 2021

--- lua/ulx/modules/sh/atmos.lua
+++ lua/ulx/modules/sh/atmos.lua
@@ -1,6 +1,6 @@
 --[[-------------------------------------------------------------------------
 Atmos ULX commands - A few ULX commands to control Atmos time & weather.
-Copyright (C) 2020 viral32111
+Copyright (C) 2020 - 2021 viral32111
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
